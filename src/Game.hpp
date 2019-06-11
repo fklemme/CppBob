d217be8452847d9b4e19f46e4f29a538a46e95b6
Florian Klemme <kruecke@kruecke.net>
2019-06-11 21:10:11 +0200

    Added workaround for little windows bug.

--- src/Game.hpp
+++ src/Game.hpp
@@ -18,8 +18,13 @@ class Game {
   void check_state();
 
  private:
+  // Handle drawing window in an own thread.
   friend void window_handler(Game*);
   std::unique_ptr<std::thread> m_thread;
+  // Have a GL context created in the main thread initially.
+  // This is just a workaround for a Windows specific problem as described here:
+  // https://en.sfml-dev.org/forums/index.php?topic=22131.msg156596#msg156596
+  sf::Context m_context;
   bool m_running;
   std::shared_ptr<Map> m_map;
   std::shared_ptr<Bob> m_bob;
@@ -30,6 +35,6 @@ class GameOver : public std::exception {
   inline GameOver(std::string msg) : m_msg(std::move(msg)) {}
   inline const char* what() const noexcept override { return m_msg.c_str(); }
 
-  private:
+ private:
   std::string m_msg;
 };
