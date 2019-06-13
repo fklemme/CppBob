46fff02f79110011db835c4799ceb5ed5e72b9c4
Florian Klemme <mail@florianklemme.de>
2019-06-13 16:20:17 +0200

    Added solutions for level 6 and 7.

--- src/solutions.hpp
+++ src/solutions.hpp
@@ -100,4 +100,127 @@ inline void level5(Bob& bob) {
   }
 }
 
+inline void level6(Bob& bob) {
+  // Helpers
+  auto try_move_left = [&]() {
+    bob.turn_left();
+    bob.move();
+    if (bob.on_marked_tile())
+      return true;  // success
+    else {
+      // recover
+      bob.turn_right();
+      bob.turn_right();
+      bob.move();
+      bob.turn_left();
+      return false;  // no success
+    }
+  };
+  auto try_move_front = [&]() {
+    bob.move();
+    if (bob.on_marked_tile())
+      return true;  // success
+    else {
+      // recover
+      bob.turn_right();
+      bob.turn_right();
+      bob.move();
+      bob.turn_left();
+      bob.turn_left();
+      return false;  // no success
+    }
+  };
+  auto try_move_right = [&]() {
+    bob.turn_right();
+    bob.move();
+    if (bob.on_marked_tile())
+      return true;  // success
+    else {
+      // recover
+      bob.turn_left();
+      bob.turn_left();
+      bob.move();
+      bob.turn_right();
+      return false;  // no success
+    }
+  };
+
+  while (true) {
+    if (try_move_front()) {
+      // nothing to do on success, we already moved
+    } else if (try_move_left()) {
+      // nothing to do on success, we already moved
+    } else if (try_move_right()) {
+      // nothing to do on success, we already moved
+    } else
+      return;  // give up
+  }
+}
+
+inline void level7(Bob& bob) {
+  // Helpers
+  auto try_move_left = [&]() {
+    bob.turn_left();
+    if (bob.wall_in_front()) {
+      bob.turn_right();  // recover
+      return false;      // no success
+    }
+    bob.move();
+    if (bob.on_marked_tile())
+      return true;  // success
+    else {
+      // recover
+      bob.turn_right();
+      bob.turn_right();
+      bob.move();
+      bob.turn_left();
+      return false;  // no success
+    }
+  };
+  auto try_move_front = [&]() {
+    if (bob.wall_in_front()) return false;  // no success
+    bob.move();
+    if (bob.on_marked_tile())
+      return true;  // success
+    else {
+      // recover
+      bob.turn_right();
+      bob.turn_right();
+      bob.move();
+      bob.turn_left();
+      bob.turn_left();
+      return false;  // no success
+    }
+  };
+  auto try_move_right = [&]() {
+    bob.turn_right();
+    if (bob.wall_in_front()) {
+      bob.turn_left();  // recover
+      return false;     // no success
+    }
+    bob.move();
+    if (bob.on_marked_tile())
+      return true;  // success
+    else {
+      // recover
+      bob.turn_left();
+      bob.turn_left();
+      bob.move();
+      bob.turn_right();
+      return false;  // no success
+    }
+  };
+
+  while (true) {
+    if (try_move_front()) {
+      // nothing to do on success, we already moved
+    } else if (try_move_left()) {
+      // nothing to do on success, we already moved
+    } else if (try_move_right()) {
+      // nothing to do on success, we already moved
+    } else
+      return;  // give up
+  }
+}
+
 }  // namespace solutions
