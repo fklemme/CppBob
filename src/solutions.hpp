5c3c268df882b6922833ed3b1c3b82d2c3fbf3e8
Florian Klemme <mail@florianklemme.de>
2019-06-20 16:30:39 +0200

    Use A* based on Wikipedia pseudocode.

--- src/solutions.hpp
+++ src/solutions.hpp
@@ -2,9 +2,8 @@
 
 #include <algorithm>
 #include <cassert>
-#include <cmath>
 #include <map>
-#include <queue>
+#include <set>
 #include <vector>
 
 #include "Bob.hpp"
@@ -268,16 +267,111 @@ inline void level8(Bob& bob) {
   }
 }
 
-inline void level9(Bob& bob) {
-  // Visualize path finding algorithm with marks on the map
-  constexpr bool debug_visualize = true;
-  const auto debug_step_delay = std::chrono::milliseconds(250);
+// ----- Level 9: Generic path finding with A* algorithm -----
+// Visualize path finding algorithm with marks on the map
+constexpr bool debug_visualize = true;
+const auto debug_step_delay = std::chrono::milliseconds(250);
 
+// Based on https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode
+std::vector<Position> reconstruct_path(const std::map<Position, Position>& cameFrom,
+                                       Position current) {
+  std::vector<Position> total_path = {current};
+  while (cameFrom.count(current)) {
+    current = cameFrom.at(current);
+    total_path.push_back(current);
+  }
+  std::reverse(total_path.begin(), total_path.end());  // start to goal
+  return total_path;
+}
+
+// Based on https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode
+std::vector<Position> A_Star(const Map& map, const Position& start, const Position& goal) {
+  // Can be used as "Infinity"
+  const int max_distance = map.height() * map.width();
+
+  // The set of nodes already evaluated
+  std::set<Position> closedSet;
+
+  // The set of currently discovered nodes that are not evaluated yet.
+  // Initially, only the start node is known.
+  std::set<Position> openSet = {start};  // TODO: use priority queue
+
+  // For each node, which node it can most efficiently be reached from.
+  // If a node can be reached from many nodes, cameFrom will eventually
+  // contain the most efficient previous step.
+  std::map<Position, Position> cameFrom;
+
+  // For each node, the cost of getting from the start node to that node.
+  std::map<Position, int> gScore;  // default value of Infinity
+
+  // The cost of going from start to start is zero.
+  gScore[start] = 0;
+
+  // For each node, the total cost of getting from the start node to the goal
+  // by passing by that node. That value is partly known, partly heuristic.
+  std::map<Position, int> fScore;  // default value of Infinity
+
+  // For the first node, that value is completely heuristic.
+  fScore[start] = distance(start, goal);
+
+  // Helper to get "Infinity" as default when accessing a map
+  auto get = [&max_distance](auto& map, const Position& p) {
+    if (!map.count(p)) map.emplace(p, max_distance);
+    return map[p];
+  };
+
+  while (!openSet.empty()) {
+    // current := the node in openSet having the lowest fScore[] value
+    auto current = *std::min_element(
+        // openSet.begin(), openSet.end(), // breadth first
+        openSet.rbegin(), openSet.rend(),  // depth first
+        [&](const Position& a, const Position& b) { return get(fScore, a) < get(fScore, b); });
+
+    if (current == goal) return reconstruct_path(cameFrom, current);
+
+    if constexpr (debug_visualize) {
+      // Mark current position for visualization (debugging)
+      const_cast<Map&>(map).tile(current, 'm');
+      std::this_thread::sleep_for(debug_step_delay);
+    }
+
+    openSet.erase(current);
+    closedSet.insert(current);
+
+    // Neighbors of current
+    Position up{current.row - 1, current.col};
+    Position down{current.row + 1, current.col};
+    Position left{current.row, current.col - 1};
+    Position right{current.row, current.col + 1};
+    for (auto& neighbor : {up, down, left, right}) {
+      if (map.tile(neighbor) == 'w') continue;  // Neighbor is wall and cannot be moved to
+
+      if (closedSet.count(neighbor)) continue;  // Ignore the neighbor which is already evaluated
+
+      // The distance from start to a neighbor
+      const int tentative_gScore = get(gScore, current) + 1;
+
+      if (!openSet.count(neighbor))  // Discover a new node
+        openSet.insert(neighbor);
+      else if (tentative_gScore >= get(gScore, neighbor))
+        continue;
+
+      // This path is the best until now. Record it!
+      cameFrom[neighbor] = current;
+      gScore[neighbor] = tentative_gScore;
+      fScore[neighbor] = tentative_gScore + distance(neighbor, goal);
+    }
+  }
+
+  return {};  // no path found
+}
+
+inline void level9(Bob& bob) {
   auto& map = *bob.map();
   Position start = bob.position();
   Position destination{map.height(), map.width()};  // initially off map
 
-  // Find destination "d"
+  // Find destination "d" on map
   for (std::size_t row = 0; row < map.height(); ++row) {
     for (std::size_t col = 0; col < map.width(); ++col) {
       if (map.tile(row, col) == 'd') destination = {row, col};
@@ -294,64 +388,8 @@ inline void level9(Bob& bob) {
     }
   }
 
-  // --- Find path from start to destination ---
-  // Compare function to get nearest Position to destination in priority queue
-  auto dist_compare = [&destination](const Position& a, const Position& b) {
-    return distance(a, destination) > distance(b, destination);
-  };
-  // Positions to check next
-  std::priority_queue<Position, std::vector<Position>, decltype(dist_compare)> positions(
-      dist_compare);
-  // Checked positions, and how did we get there?
-  std::map<Position, Position> predecessors;
-  predecessors.emplace(start, start);  // let start be its own predecessor, used later
-
-  // Add start as first position to check
-  positions.push(start);
-
-  // Route to destination
-  std::vector<Position> dest_route;
-
-  // Path finding algorithm
-  while (!positions.empty()) {
-    // Check next position in queue
-    Position current = positions.top();
-    positions.pop();
-
-    // If we reached the destination...
-    if (current == destination) {
-      // Build route backwards from destination to start
-      Position p = current;
-      // Only for start, its predecessor will be itself
-      while (p != predecessors[p]) {
-        dest_route.push_back(p);
-        p = predecessors[p];
-      }
-      // Reverse order: Now from start to destination (start itself is not in route)
-      std::reverse(dest_route.begin(), dest_route.end());
-      // End path finding!
-      break;
-    }
-
-    if constexpr (debug_visualize) {
-      // Mark current position for visualization (debugging)
-      const_cast<Map&>(map).tile(current, 'm');
-      std::this_thread::sleep_for(debug_step_delay);
-    }
-
-    // Add ajecent positions that are not already checked
-    Position up{current.row - 1, current.col};
-    Position down{current.row + 1, current.col};
-    Position left{current.row, current.col - 1};
-    Position right{current.row, current.col + 1};
-    for (auto& p : {up, down, left, right}) {
-      // TODO: Add boundary checks
-      if (map.tile(p) != 'w' && predecessors.count(p) == 0) {
-        positions.push(p);
-        predecessors.emplace(p, current);
-      }
-    }
-  }  // end of path finding
+  // Use path finding algorithm to find best path to destination
+  auto route = A_Star(map, start, destination);
 
   if constexpr (debug_visualize) {
     // Clear all marks, again
@@ -362,20 +400,21 @@ inline void level9(Bob& bob) {
     }
 
     // Visualize route with marks
-    auto mark_route = dest_route;
+    auto mark_route = route;
     mark_route.pop_back();  // don't override the destination itself
-    for (auto& p : mark_route) const_cast<Map&>(map).tile(p, 'm');
+    for (auto& position : mark_route) const_cast<Map&>(map).tile(position, 'm');
   }
 
   // Follow the route to destination
-  for (auto& p : dest_route) {
+  route.erase(route.begin());  // ignore first position: Bob is on that tile
+  for (auto& postion : route) {
     // Figure out target direction
     Orientation target_direction;
-    if (p.row == bob.position().row - 1)
+    if (postion.row == bob.position().row - 1)
       target_direction = Orientation::up;
-    else if (p.row == bob.position().row + 1)
+    else if (postion.row == bob.position().row + 1)
       target_direction = Orientation::down;
-    else if (p.col == bob.position().col - 1)
+    else if (postion.col == bob.position().col - 1)
       target_direction = Orientation::left;
     else
       target_direction = Orientation::right;
